0c6f98fcae5e9b28f36110b44b7fecd85b2e86df
shani <ishanranasingh@gmail.com>
2021-09-24 08:40:28 +0530

    secret keys hidden

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv 
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-un74buw=4xq@7i=3-pe#auui$t%_s*@%s%zxu9!m^vgu&t@jc8'
+SECRET_KEY = os.environ.get("secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +81,8 @@ DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "newsfeed",
-        "USER": "ishan",
-        "PASSWORD": "Chintu@98",
+        "USER": os.environ.get("user"),
+        "PASSWORD": os.environ.get("password"),
         'HOST': 'localhost'
     }
 }
