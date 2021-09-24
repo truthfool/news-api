5c3858f015599410f1672ed84fc79ef7b9b69008
shani <ishanranasingh@gmail.com>
2021-09-24 10:00:58 +0530

    removed postgres dependency

--- core/core/settings.py
+++ core/core/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "newsfeed",
-        "USER": os.environ.get("user"),
-        "PASSWORD": os.environ.get("password"),
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
