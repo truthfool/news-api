b38e39755c0462e4bf75468ba59deb135d12ae5d
shani <ishanranasingh@gmail.com>
2021-09-27 18:03:03 +0530

    changes in base

--- core/news/views.py
+++ core/news/views.py
@@ -4,20 +4,29 @@ import requests
 import json
 import os
 from dotenv import load_dotenv 
+import sqlite3
 
 load_dotenv()
-# Create your views here.
-def API_data():
-    try:
-        url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/TrendingNewsAPI?pageNumber=1&pageSize=10&withThumbnails=false&location=IN"
 
-        payload={}
+def API_data(search_query=None):
+    try:
+        
+
+        querystring = {"q":search_query,"pageNumber":"1","pageSize":"10","autoCorrect":"true","fromPublishedDate":"null","toPublishedDate":"null"}
+
         headers = {
-            'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
+            'x-rapidapi-host': "contextualwebsearch-websearch-v1.p.rapidapi.com",
             'x-rapidapi-key': os.environ.get("api_key")
         }
 
-        response = requests.request("GET", url, headers=headers, data=payload)
+        if search_query:
+            url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI"
+            
+            response = requests.request("GET", url, headers=headers, params=querystring)
+        else:
+            url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/TrendingNewsAPI?pageNumber=1&pageSize=10&withThumbnails=false&location=IN"
+
+            response = requests.request("GET", url, headers=headers)
 
         response_data=json.loads(response.text)
 
@@ -28,7 +37,7 @@ def API_data():
 
 def Home(request):
     
-    company=request.GET.get('company')
+    company=request.POST.get('id_type')
 
     if company:
         news_list=NewsModel.objects.filter(dictionary_tokens=company)
@@ -40,6 +49,8 @@ def Home(request):
 
 def all_news(request):
     
-    response_data=API_data()
+    query=request.GET.get('search_query')
+
+    response_data=API_data(query)
 
     return render(request,'all_news.html',context={'articles':response_data})
